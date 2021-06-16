73a2ad26d57ed288b617cb75a945509ee9a0e700
PoRuiHuang <B06705027@ntu.edu.tw>
2021-06-16 10:21:30 +0800

    Search Done

--- Backend/api/api.py
+++ Backend/api/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import json
-import datetime
+from datetime import datetime
 from flask_cors import CORS, cross_origin
 import mysql.connector
 import sys
@@ -20,13 +20,105 @@ def query_result():
     Date = post_data.get('Date')
     Type = post_data.get('Type')
     Content = post_data.get('Content')
-    
+    Evidence = post_data.get('Evidence')
+
+    # print(Date)
+
+    date_object = datetime.strptime(Date, '%b %d %Y %H:%M:%S')
+
+
+    cnx = mysql.connector.connect(user='root', password='123456', host='35.229.151.61', database='reports')
+    cursor = cnx.cursor()
+    sql = "INSERT INTO report (reporter, reporter_email, person, type, content, evidence, date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = (Reporter, Mail, Person, Type, Content, Evidence, date_object.strftime('%Y-%m-%d %H:%M:%S'))
+    try:
+        cursor.execute(sql, val)
+        cnx.commit()
+
+        message = {'status': 'Success'}
+        return jsonify(message)
+
+    except (mysql.connector.Error, mysql.connector.Warning) as e:
+        print(e)
+        return None
+
+
     # message = {'status': 'Success'}
     # print(Reporter, Mail, Person, Date, Type, Content)
 
     
+@app.route('/query_search', methods=['POST'])
+def query_search():
+    post_data = request.get_json()
+    Mail = post_data.get('Mail')
+    Person = post_data.get('Person')
+    StartDate = post_data.get('StartDate')
+    EndDate = post_data.get('EndDate')
+    Type = post_data.get('Type')
+    
+    Type_Query = ""
+    Date_Query = ""
+    Person_Query = ""
+    Mail_Query = ""
 
+    # print("A" + Type + "A")
+
+    if Type == "none":
+        Type_Query = " type IS NOT NULL"
+    else:
+        Type_Query = " type = \'" + Type + "\'"
+
+    if StartDate is not "":
+        StartDate = StartDate[:12] + "00:00:00"
+        EndDate = EndDate[:12] + "23:59:59"
+        START = datetime.strptime(StartDate, '%b %d %Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+        END = datetime.strptime(EndDate, '%b %d %Y %H:%M:%S').strftime('%Y-%m-%d %H:%M:%S')
+
+        Date_Query = " AND date BETWEEN \'" + START + "\' AND \'" + END + "\'" 
+
+    if Person is not "":
+        Person_Query = " AND person = \'" + Person + "\'"
+
+    if Mail is not "":
+        Mail_Query = " AND reporter_email = \'" + Mail + "\'" 
+
+
+    cnx = mysql.connector.connect(user='root', password='123456', host='35.229.151.61', database='reports')
+    cursor = cnx.cursor(buffered=True)
+    sql = "SELECT * FROM `report` WHERE" + Type_Query + Date_Query + Person_Query + Mail_Query
+
+    # print(sql)
+    try:
+        cursor.execute(sql)
+        cnx.commit()
+        result = cursor.fetchall()
+
+        row_list = []
+        for row in result:
+            ele_row = {
+                "Reporter" : row[0],
+                "Email" : row[1],
+                "Person": row[2],
+                "Type": row[3],
+                "Content": row[4],
+                "Evidence": row[5],
+                "Date": row[6]
+            }
+            row_list.append(ele_row)
+
+
+
+        message = {'status': 'Success'}
+        return json.dumps(row_list, default= str)
+
+    except (mysql.connector.Error, mysql.connector.Warning) as e:
+        print(e)
+        return None
+
+
+    message = {'status': 'Success'}
     return jsonify(message)
+
 
 
 # @app.route('/')
--- Search/src/axios.js
+++ Search/src/axios.js
@@ -2,42 +2,44 @@ import axios from 'axios'
 
 const uploadSearch = (values) => {
     
-    var Mail = ""
-    var Person = ""
-    var Startdate = ""
-    var Enddate = ""
-    var Type = values.type
+    let Mail = ""
+    let Person = ""
+    let StartDate = ""
+    let EndDate = ""
+    let Type = values.type
 
-    if(!values.reporterMail) {
+    if(values.reporterMail) {
         Mail = values.reporterMail
     }
-    if(!values.reportedPerson) {
+    if(values.reportedPerson) {
       Person = values.reportedPerson
     }
-    if(!values.rangeDate[0]._d.toString().substring(4, 24)) {
-      Startdate = values.rangeDate[0]._d.toString().substring(4, 24)
-    }
-    if(!values.rangeDate[1]._d.toString().substring(4, 24)) {
-      Enddate = values.rangeDate[1]._d.toString().substring(4, 24)
+    if(values.rangeDate) {
+      StartDate = values.rangeDate[0]._d.toString().substring(4, 24)
+      EndDate = values.rangeDate[1]._d.toString().substring(4, 24)
     }
     
     // TODO (全部都修好了，改完api網址幫我把最下面的return success改掉)
-    // axios.post('http://127.0.0.1:8000/query_result', {
-    //     Mail: Mail,
-    //     Person: Person,
-    //     Startdate: Startdate,
-    //     Enddate: Enddate,
-    //     Type: Type
-    // })
-    // .then((response) => {
-    //     //發送請求成功時
-    //     var status = response.data.status;
-    //     console.log(status)
-    //     return('success')
-    // }).catch((error) => {
-    //     //發送請求失敗時
-    //     return('fail')
-    // });
+    axios.post('http://127.0.0.1:8000/query_search', {
+        Mail: Mail,
+        Person: Person,
+        StartDate: StartDate,
+        EndDate: EndDate,
+        Type: Type
+    })
+    .then((response) => {
+        //發送請求成功時
+        let data = response.data;
+
+        //return data is an array of dic, data[0] means the first dictionary element, data[0].Reporter means the Reporter in first dictionary element
+
+        console.log(data)
+        // console.log(data[2].Reporter)
+        return('success')
+    }).catch((error) => {
+        //發送請求失敗時
+        return('fail')
+    });
 
     return("success")
 }
